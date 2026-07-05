7a02572fa66a3659a29bf5b8d321d1c248447489
Fantasy <1362724990@qq.com>
2026-07-05 17:08:57 +0800

    refactor: 优化判题逻辑和接口迁移

--- src/main/java/com/fantasy/fuoj/controller/QuestionController.java
+++ src/main/java/com/fantasy/fuoj/controller/QuestionController.java
@@ -11,10 +11,15 @@ import com.fantasy.fuoj.constant.UserConstant;
 import com.fantasy.fuoj.exception.BusinessException;
 import com.fantasy.fuoj.exception.ThrowUtils;
 import com.fantasy.fuoj.model.dto.question.*;
+import com.fantasy.fuoj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.fantasy.fuoj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.fantasy.fuoj.model.entity.Question;
+import com.fantasy.fuoj.model.entity.QuestionSubmit;
 import com.fantasy.fuoj.model.entity.User;
+import com.fantasy.fuoj.model.vo.QuestionSubmitVO;
 import com.fantasy.fuoj.model.vo.QuestionVO;
 import com.fantasy.fuoj.service.QuestionService;
+import com.fantasy.fuoj.service.QuestionSubmitService;
 import com.fantasy.fuoj.service.UserService;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
@@ -38,6 +43,9 @@ public class QuestionController {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private QuestionSubmitService questionSubmitService;
 
     private static final Gson GSON = new Gson();
 
@@ -282,5 +290,42 @@ public class QuestionController {
         }
         boolean result = questionService.updateById(question);
         return ResultUtils.success(result);
+    }
+
+    /**
+     * 提交题目
+     *
+     * @param questionSubmitAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/question_submit/do")
+    public BaseResponse<Long> doQuestionSubmit(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
+                                               HttpServletRequest request) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
+        return ResultUtils.success(questionSubmitId);
+    }
+
+    /**
+     * 分页获取题目提交列表（除了管理员外，普通用户只能看到非答案，提交代码等公开信息）
+     *
+     * @param questionSubmitQueryRequest
+     * @return
+     */
+    @PostMapping("/question_submit/list/page")
+    public BaseResponse<Page<QuestionSubmitVO>> listQuestionSubmitByPage(@RequestBody QuestionSubmitQueryRequest questionSubmitQueryRequest, HttpServletRequest request) {
+        long current = questionSubmitQueryRequest.getCurrent();
+        long size = questionSubmitQueryRequest.getPageSize();
+        // 从数据库中查询原始的题目提交分类信息
+        Page<QuestionSubmit> questionSubmitPage = questionSubmitService.page(new Page<>(current, size),
+                questionSubmitService.getQueryWrapper(questionSubmitQueryRequest));
+        final User loginUser = userService.getLoginUser(request);
+        // 返回脱敏信息
+        return ResultUtils.success(questionSubmitService.getQuestionSubmitVOPage(questionSubmitPage, loginUser));
     }
 }
--- src/main/java/com/fantasy/fuoj/judge/strategy/JavaLanguageJudgeStrategy.java
+++ src/main/java/com/fantasy/fuoj/judge/strategy/JavaLanguageJudgeStrategy.java
@@ -8,6 +8,7 @@ import com.fantasy.fuoj.model.entity.Question;
 import com.fantasy.fuoj.model.enums.JudgeInfoMessageEnum;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Java 程序的判题策略
@@ -23,8 +24,8 @@ public class JavaLanguageJudgeStrategy implements JudgeStrategy {
     @Override
     public JudgeInfo doJudge(JudgeContext judgeContext) {
         JudgeInfo judgeInfo = judgeContext.getJudgeInfo();
-        Long memory = judgeInfo.getMemory();
-        Long time = judgeInfo.getTime();
+        Long memory = Optional.ofNullable(judgeInfo.getMemory()).orElse(0L);
+        Long time = Optional.ofNullable(judgeInfo.getTime()).orElse(0L);
         List<String> inputList = judgeContext.getInputList();
         List<String> outputList = judgeContext.getOutputList();
         Question question = judgeContext.getQuestion();
@@ -42,7 +43,7 @@ public class JavaLanguageJudgeStrategy implements JudgeStrategy {
         // 依次判断每一项输出和预期输出是否相等
         for (int i = 0; i < judgeCaseList.size(); i++) {
             JudgeCase judgeCase = judgeCaseList.get(i);
-            if (judgeCase.getOutput().equals(outputList.get(i))) {
+            if (!judgeCase.getOutput().equals(outputList.get(i))) {
                 judgeInfoMessageEnum = JudgeInfoMessageEnum.WRONG_ANSWER;
                 judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
                 return judgeInfoResponse;
